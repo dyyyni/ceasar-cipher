ea002f8ed711faef7117ed0f1c5cce514e552e88
Daniel Luoma <luomadaniel95@gmail.com>
2023-09-01 13:44:54 +0300

    The encryption and decryption functions are ready
    Made for ascii characters only.
    Wraparound with modulo works.

---
+++ ceasar-cipher.py
@@ -0,0 +1,19 @@
+def ceasarEncrypt(inString: str, key: int):
+    encString = ""
+    for char in inString:
+        if char.isalpha():
+            offset = ord('A') if char.isupper() else ord('a')
+            encString += chr( (ord(char) - offset + key) % 26 + offset)
+        else:
+            encString += char
+    return encString
+
+def ceasarDecrypt(inString: str, key: int):
+    decString = ""
+    for char in inString:
+        if char.isalpha():
+            offset = ord('A') if char.isupper() else ord('a')
+            decString += chr( (ord(char) - offset - key) % 26 + offset) 
+        else:
+            decString += char
+    return decString
