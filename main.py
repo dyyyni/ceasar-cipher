50925748ba85b04ba0624c43cdc550893bdfb605
Daniel Luoma <luomadaniel95@gmail.com>
2023-09-02 14:04:59 +0300

    Added double encrypt and double decrypt

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import sys
 from ceasar_cipher import ceasarDecrypt, ceasarEncrypt
 
+def encrypt(inString: str) -> str:
+    properKey = False
+    while(not properKey):
+        try:
+            inKey = int(input("Enter the Ceasar cipher key: "))
+            properKey = True
+        except ValueError:
+            print("Enter the key as a number.")
+    return ceasarEncrypt(inString, inKey)
+
+def decrypt(inString: str) -> str:
+    properKey = False
+    while(not properKey):
+        try:
+            inKey = int(input("Enter your guess for the key: "))
+            properKey = True
+        except ValueError:
+            print("Enter the key as a number.")
+    return ceasarDecrypt(inString, inKey)
+
+
 def main():
     modes = {
-            "-e": "encrypt",
-            "-d": "decrypt",
-            "-b": "brute force"
+            "-e"    : "encrypt",
+            "-d"    : "decrypt",
+            "-b"    : "brute force",
+            "-de"   : "double encrypt",
+            "-dd"   : "double decrypt"
             }
 
     mode = ""
@@ -23,30 +46,10 @@ def main():
     match mode:
 
         case "-e":
-            inString = input("Enter the text to encrypt: ")
-            properKey = False
-            while(not properKey):
-                try:
-                    inKey = int(input("Enter the Ceasar cipher key: "))
-                    properKey = True
-                except ValueError:
-                    print("Enter the key as a number.")
-
-            encText = ceasarEncrypt(inString, inKey)
-            print(encText)
+            print( encrypt(input("Enter the text to encrypt: ")) )
 
         case "-d":
-            inString = input("Enter the text to decrypt: ")
-            properKey = False
-            while(not properKey):
-                try:
-                    inKey = int(input("Enter your guess for the key: "))
-                    properKey = True
-                except ValueError:
-                    print("Enter the key as a number.")
-
-            decText = ceasarDecrypt(inString, inKey)
-            print(decText)
+            print( decrypt(input("Enter the text to decrypt: ")) )
 
         case "-b":
             inString = input("Enter the text to decrypt: ")
@@ -54,6 +57,13 @@ def main():
             for i in range(26):
                 decCandidate = ceasarDecrypt(inString, i)
                 print(str(i) + ": " + decCandidate)
+
+        case "-de":
+            inString = input("Enter the text to encrypt: ")
+            print( encrypt(encrypt(inString)) )
+        case "-dd":
+            inString = input("Enter the text to decrypt: ")
+            print( decrypt(decrypt(inString)) )
 
     return
 
